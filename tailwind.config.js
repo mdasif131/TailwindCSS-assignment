b1695b40a6ef64dbdf91243a98933b5e3165d98f
mdasif131 <asifmdasifchy@gmail.com>
2025-01-15 14:15:01 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*'],
+  theme: {
+    extend: {
+      fontFamily: {
+        fontPlus: ['"Plus Jakarta Sans", serif'],
+      },
+      screens: {
+        mobile: '300px',
+      },
+
+    },
+  },
+  plugins: [],
+};
